22ee5ac657ed1c372cd30c05029e44d09a552b5b
MagyarZoli <magyarz95@gmail.com>
2024-03-22 17:11:01 +0100

    ConversionRate

---
+++ src/main/java/org/example/model/conversion/ConversionRate.java
@@ -0,0 +1,35 @@
+package org.example.model.conversion;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class ConversionRate {
+
+  /**
+   * Source currency.
+   */
+  private String fromCurrency;
+
+  /**
+   * Target currency.
+   */
+  private String toCurrency;
+
+  /**
+   * Current conversion rate from the source currency to the target currency.
+   */
+  private Number convRate;
+
+  @JsonCreator
+  public ConversionRate(
+      @JsonProperty(value = "fromCurrency") String fromCurrency,
+      @JsonProperty(value = "toCurrency") String toCurrency,
+      @JsonProperty(value = "convRate") Number convRate
+  ) {
+    this.fromCurrency = fromCurrency;
+    this.toCurrency = toCurrency;
+    this.convRate = convRate;
+  }
+}
