8e1adb0a4c61a40f4438340028bfcfb4a515a267
MagyarZoli <magyarz95@gmail.com>
2024-04-08 06:17:09 +0200

    InstrumentDetails

---
+++ src/main/java/org/example/models/instruments/InstrumentDetails.java
@@ -0,0 +1,71 @@
+package org.example.models.instruments;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.example.models.AccountCode;
+
+@Data
+public class InstrumentDetails {
+
+  /**
+   * Unique code of an account.
+   */
+  private AccountCode account;
+
+  /**
+   * Instrument type as configured on the platform (e.g. INDEX).
+   */
+  private InstrumentType type;
+
+  /**
+   * Unique instrument symbol.
+   */
+  private String symbol;
+
+  /**
+   * Trading permissions of the account for the instrument. One of FULL, CLOSE_ONLY, NO.
+   */
+  private TradingStatus tradingStatus;
+
+  /**
+   * Minimum order size for an instrument.
+   */
+  private Number minOrderSize;
+
+  /**
+   * Maximum order size for an instrument.
+   */
+  private LimitValue maxOrderSize;
+
+  /**
+   * Order size increment for an instrument (e.g. 0.0001).
+   */
+  private Number minOrderSizeIncrement;
+
+  /**
+   * Margin rate for an instrument.
+   */
+  private MarginRate marginRate;
+
+  @JsonCreator
+  public InstrumentDetails(
+      @JsonProperty(value = "account") AccountCode account,
+      @JsonProperty(value = "type") InstrumentType type,
+      @JsonProperty(value = "symbol") String symbol,
+      @JsonProperty(value = "tradingStatus") TradingStatus tradingStatus,
+      @JsonProperty(value = "minOrderSize") Number minOrderSize,
+      @JsonProperty(value = "maxOrderSize") LimitValue maxOrderSize,
+      @JsonProperty(value = "minOrderSizeIncrement") Number minOrderSizeIncrement,
+      @JsonProperty(value = "marginRate") MarginRate marginRate
+  ) {
+    this.account = account;
+    this.type = type;
+    this.symbol = symbol;
+    this.tradingStatus = tradingStatus;
+    this.minOrderSize = minOrderSize;
+    this.maxOrderSize = maxOrderSize;
+    this.minOrderSizeIncrement = minOrderSizeIncrement;
+    this.marginRate = marginRate;
+  }
+}
