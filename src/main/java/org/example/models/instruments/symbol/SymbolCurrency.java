27dbc6a135de84cedf6e9ddfd900a696116ce268
MagyarZoli <magyarz95@gmail.com>
2024-04-20 08:15:08 +0200

    SymbolCurrency

---
+++ src/main/java/org/example/models/instruments/symbol/SymbolCurrency.java
@@ -0,0 +1,70 @@
+package org.example.models.instruments.symbol;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public enum SymbolCurrency
+implements Symbol {
+
+  ADA,
+  AUD_S("AUD$"),
+  BCH_S("BCH$"),
+  BTC_S("BTC$"),
+  CAD_S("CAD$"),
+  CHF_S("CHF$"),
+  CNH_S("CNH$"),
+  CNY_S("CNY$"),
+  COCOA,
+  COFFEE,
+  CORN,
+  CZK_S("CZK$"),
+  DASH_S("DASH$"),
+  DKK_S("DKK$"),
+  DOGE_S("DOGE$"),
+  DOT,
+  EOS_S("EOS$"),
+  ETC_S("ETC$"),
+  ETH_S("ETH$"),
+  EUR_S("EUR$"),
+  GBP_S("GBP$"),
+  HKD_S("HKD$"),
+  HUF_S("HUF$"),
+  ILS_S("ILS$"),
+  INR_S("INR$"),
+  JPY_S("JPY$"),
+  KRW_S("KRW$"),
+  LTC_S("LTC$"),
+  MXN_S("MXN$"),
+  NEO,
+  NOK_S("NOK$"),
+  NZD_S("NZD$"),
+  PLN_S("PLN$"),
+  RUB_S("RUB$"),
+  SEK_S("SEK$"),
+  SGD_S("SGD$"),
+  SKK_S("SKK$"),
+  SOYBEAN,
+  THB_S("THB$"),
+  TRY_S("TRY$"),
+  UAH_S("UAH$"),
+  USD_S("USD$"),
+  USDT_S("USDT$"),
+  WHEAT,
+  XDG_S("XDG$"),
+  XLM_S("XLM$"),
+  XMR_S("XMR$"),
+  XRP_S("XRP$"),
+  ZAR_S("ZAR$");
+
+  private String name;
+
+  @Override
+  public String toString() {
+    if (this.name == null) {
+      return super.name();
+    }
+    return this.name;
+  }
+}
