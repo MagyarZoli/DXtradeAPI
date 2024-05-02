e5422086870f64e9da5a7d54ecbf0ab97b7ff85c
MagyarZoli <magyarz95@gmail.com>
2024-05-02 06:25:24 +0200

    TransactionType

---
+++ src/main/java/org/example/models/accounts/TransactionType.java
@@ -0,0 +1,16 @@
+package org.example.models.accounts;
+
+public enum TransactionType {
+
+  COMMISSION,
+  FINANCING,
+  DEPOSIT,
+  WITHDRAWAL,
+  COST,
+  SETTLEMENT,
+  ADJUSTMENT,
+  EX_DIVIDEND,
+  REBATE,
+  NEGATIVE_BALANCE_CORRECTION,
+  BUST;
+}
