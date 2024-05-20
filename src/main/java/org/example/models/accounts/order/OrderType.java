ed7321c2903c30e948b3e523030bc1f37e4eddb4
MagyarZoli <magyarz95@gmail.com>
2024-05-20 10:31:51 +0200

    OrderType

---
+++ src/main/java/org/example/models/accounts/order/OrderType.java
@@ -0,0 +1,12 @@
+package org.example.models.accounts.order;
+
+/**
+ * Order type cannot be changed after the order is issued. The value must be omitted in ‘Replace’ requests except group
+ * requests, which may contain new orders in Replace requests.
+ */
+public enum OrderType {
+
+  MARKET,
+  LIMIT,
+  STOP
+}
