b3de441488ed786a68c3d0e344cd352dfb4e451d
MagyarZoli <magyarz95@gmail.com>
2024-07-16 06:04:16 +0200

    Model 1.1

--- src/main/java/org/example/models/Model.java
+++ src/main/java/org/example/models/Model.java
@@ -1,3 +1,18 @@
 package org.example.models;
 
-public interface Model {}
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.exception.APIException;
+import org.json.JSONObject;
+
+public interface Model {
+
+  default JSONObject parseToJSONObject() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      return new JSONObject(objectMapper.writeValueAsString(this));
+    } catch (JsonProcessingException e) {
+      throw new APIException(e.getMessage(), e);
+    }
+  }
+}
